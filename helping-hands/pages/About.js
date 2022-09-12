abf21602fef0dd9c50c11a45088f6d5538ee580b
Abdulrahman Mohammed <95183257+refa3e99@users.noreply.github.com>
2022-09-12 14:26:35 +0300

    some fixes

--- helping-hands/pages/About.js
+++ helping-hands/pages/About.js
@@ -1,12 +1,9 @@
-import FQAs from '../components/FQAs'
-
 export default function About() {
     return (
         <>
           <div className="center">
           <h1>This is the about page</h1>
           </div>
-          <FQAs/>
             
         </>
     )
