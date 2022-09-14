fbdb82775c9f2e249a007047f362a8f63e7555c9
MalakOdtalla <malakodt35@gmail.com>
2022-09-14 11:15:22 +0300

    edit components

--- helping-hands/components/CreatedCampiagns.js
+++ helping-hands/components/CreatedCampiagns.js
@@ -1,49 +1,101 @@
 
 export default function CreatedCampiagns() {
-  return (
-    <>
+  <>
+  <h3 class="w-full flex justify-center flex-row flex-wrap mx-auto">User joined campaigns </h3>
+  <section class="w-full flex justify-center flex-row flex-wrap mx-auto">
+       <div class="transition-all duration-150 flex w-full px-4 py-6 md:w-1/2 lg:w-1/3">
+         <div class="flex flex-col items-stretch min-h-full pb-4 mb-6 transition-all duration-150 bg-white rounded-lg shadow-lg hover:shadow-2xl">
+           <div class="md:flex-shrink-0">
+             <img
+               src="https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder-1024x512.png"
+               alt="Blog Cover"
+               class="object-fill w-full rounded-lg rounded-b-none md:h-56"
+             />
+           </div>
+           <div class="flex items-center justify-between px-4 py-2 overflow-hidden">
+             <span class="text-xs font-medium text-blue-600 uppercase">
+               category
+             </span>
+             <div class="flex flex-row items-center">
+               <div class="text-xs font-medium text-gray-500 flex flex-row items-center mr-2">
+                 <svg
+                   class="w-4 h-4 mr-1"
+                   fill="none"
+                   stroke="currentColor"
+                   viewBox="0 0 24 24"
+                   xmlns="http://www.w3.org/2000/svg"
+                 >
+                   <path
+                     stroke-linecap="round"
+                     stroke-linejoin="round"
+                     stroke-width="2"
+                     d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+                   ></path>
+                 </svg>
+                 <span>25</span>
+               </div>
 
-
-<div class="max-w-2xl mx-auto">
-
-	<form>
-		<div class="relative z-0 mb-6 w-full group">
-			<input type="email" name="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-			<label for="floating_email" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
-		</div>
-		<div class="relative z-0 mb-6 w-full group">
-			<input type="password" name="floating_password" id="floating_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-			<label for="floating_password" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
-		</div>
-		<div class="relative z-0 mb-6 w-full group">
-			<input type="password" name="repeat_password" id="floating_repeat_password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-			<label for="floating_repeat_password" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm password</label>
-		</div>
-		<div class="grid xl:grid-cols-2 xl:gap-6">
-			<div class="relative z-0 mb-6 w-full group">
-				<input type="text" name="floating_first_name" id="floating_first_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-				<label for="floating_first_name" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">First name</label>
-			</div>
-			<div class="relative z-0 mb-6 w-full group">
-				<input type="text" name="floating_last_name" id="floating_last_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-				<label for="floating_last_name" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
-			</div>
-		</div>
-		<div class="grid xl:grid-cols-2 xl:gap-6">
-			<div class="relative z-0 mb-6 w-full group">
-				<input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-				<label for="floating_phone" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number (123-456-7890)</label>
-			</div>
-			<div class="relative z-0 mb-6 w-full group">
-				<input type="text" name="floating_company" id="floating_company" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-				<label for="floating_company" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Company (Ex. Google)</label>
-			</div>
-		</div>
-		<button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-	</form>
-
-</div>
-
-    </>
-  )
+               <div class="text-xs font-medium text-gray-500 flex flex-row items-center">
+                 <svg
+                   class="w-4 h-4 mr-1"
+                   fill="none"
+                   stroke="currentColor"
+                   viewBox="0 0 24 24"
+                   xmlns="http://www.w3.org/2000/svg"
+                 >
+                   <path
+                     stroke-linecap="round"
+                     stroke-linejoin="round"
+                     stroke-width="2"
+                     d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"
+                   ></path>
+                 </svg>
+                 <span>7</span>
+               </div>
+             </div>
+           </div>
+           <hr class="border-gray-300" />
+           <div class="flex flex-wrap items-center flex-1 px-4 py-1 text-center mx-auto">
+             <a href="#" class="hover:underline">
+               <h2 class="text-2xl font-bold tracking-normal text-gray-800">
+                 {/* {campaign.campaign.title} */}
+               </h2>
+             </a>
+           </div>
+           <hr class="border-gray-300" />
+           <p class="flex flex-row flex-wrap w-full px-4 py-2 overflow-hidden text-sm text-justify text-gray-700">
+             Lorem ipsum dolor sit amet consectetur adipisicing elit.
+             Alias, magni fugiat, odit incidunt necessitatibus aut nesciunt
+             exercitationem aliquam id voluptatibus quisquam maiores
+             officia sit amet accusantium aliquid quo obcaecati quasi.
+           </p>
+           <hr class="border-gray-300" />
+           <section class="px-4 py-2 mt-2">
+             <div class="flex items-center justify-between">
+               <div class="flex items-center flex-1">
+                 <img
+                   class="object-cover h-10 rounded-full"
+                   src="https://thumbs.dreamstime.com/b/default-avatar-photo-placeholder-profile-icon-eps-file-easy-to-edit-default-avatar-photo-placeholder-profile-icon-124557887.jpg"
+                   alt="Avatar"
+                 />
+                 <div class="flex flex-col mx-2">
+                   <a
+                     href=""
+                     class="font-semibold text-gray-700 hover:underline"
+                   >
+                     Username
+                   </a>
+                   <span class="mx-1 text-xs text-gray-600">
+                     28 Sep 2020
+                   </span>
+                 </div>
+               </div>
+               <p class="mt-1 text-xs text-gray-600">9 minutes ago</p>
+             </div>
+           </section>
+         </div>
+       </div>
+     </section>
+ 
+</>
 }
