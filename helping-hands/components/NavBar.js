fa169bac7c1395ac7a591de822ce404a7325eaa5
MalakOdtalla <malakodt35@gmail.com>
2022-09-20 22:04:56 +0300

    testing

--- helping-hands/components/NavBar.js
+++ helping-hands/components/NavBar.js
@@ -8,6 +8,11 @@ import { useState } from 'react'
 
 import useSWR from "swr";
 import axios from "axios";
+import Router from "next/router";
+// import Dropdown from 'react-bootstrap/Dropdown';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 
 
@@ -15,17 +20,18 @@ export default function Header(props) {
   const campaignUrl = 'https://helping-hands-api.herokuapp.com/api/v1/campaign/';
   const fetcher = async (campaignUrl) => await axios.get(campaignUrl).then((res) => res.data);
   const { data, error } = useSWR(campaignUrl, fetcher);
-  console.log(data)
+  // console.log(data)
   const [filteredData, setFilteredData] = useState([]);
   const [wordEntered, setWordEntered] = useState("");
   const { tokens, logout } = useAuth()
 
   const handleFilter = (event) => {
-    const searchWord = event.target.value;
-    setWordEntered(searchWord);
-    const newFilter = data && data.filter((value) => {
-      return value.title.toLowerCase().includes(searchWord.toLowerCase());
+      const searchWord = event.target.value;
+      setWordEntered(searchWord);
+      const newFilter = data && data.filter((value) => {
+          return value.title.toLowerCase().includes(searchWord.toLowerCase());
     });
+ 
 
     if (searchWord === "") {
       setFilteredData([]);
@@ -34,9 +40,9 @@ export default function Header(props) {
     }
   };
 
-  const clearInput = () => {
-    setFilteredData([]);
-    setWordEntered("");
+    const clearInput = () => {
+      setFilteredData([]);
+      setWordEntered("");
   };
 
 
@@ -53,97 +59,73 @@ export default function Header(props) {
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
               Helping Hands
             </span>
+  
           </a>
           <div className="flex md:order-2">
-            <button
-              type="button"
-              data-collapse-toggle="navbar-search"
-              aria-controls="navbar-search"
-              aria-expanded="false"
-              className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
-            >
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-              <span className="sr-only">Search</span>
-            </button>
-            <div className="relative hidden md:block"  value={wordEntered}   onChange={handleFilter}>
-              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <svg
-                  className="w-5 h-5 text-gray-500"
-                  aria-hidden="true"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                    clip-rule="evenodd"
-                  ></path>
-                </svg>
-                <span className="sr-only">Search icon</span>
-              </div>
+     
+            <div className="relative inline-block "  value={wordEntered}   onChange={handleFilter}>
+                <div className="searchIcon">
+              {filteredData.length === 0 ? (
+                      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                      <svg
+                        className="w-5 h-5 text-gray-500"
+                        aria-hidden="true"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                          clip-rule="evenodd"
+                        ></path>
+                      </svg>
+                    </div>
+              ) : (
+                <button  class="cursor-pointer" onClick={clearInput} />
+              )}
+            </div>
+        
+              <form action="/SearchModal" class="flex flex-wrap justify-between md:flex-row">
+                 
               <input
                 type="text"
                 id="search-navbar"
                 className="block w-full p-2 pl-10 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                placeholder="Search..."
+                placeholder="Search Campaigns"
                 wtx-context="280DA549-C6B7-4D0E-84AA-AD2F93DE9FE2"
-              ></input>
+              ></input></form>
+                        {/* <Dropdown>
+                              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                Dropdown Button
+                              </Dropdown.Toggle>
+                        
+                              <Dropdown.Menu>
+                                <Dropdown.Item  href="#/action-1"> title</Dropdown.Item>
+                                <Dropdown.Item href="#/action-2"> value</Dropdown.Item>
+                                <Dropdown.Item href="#/action-3"> name</Dropdown.Item>
+                              </Dropdown.Menu>
+                            </Dropdown> */}
+            
                 {filteredData.length != 0 && (
-        <div >
-          {filteredData.slice(0, 15).map((value, key) => {
-            return (
-              <a href={value.description}>
-                <p>{value.title} </p>
-              </a>
-            );
-          })}
-        </div>
-        )}
-            </div>
+                        <div>
+                          {filteredData.slice(0,10).map((value, key) => {
+                            return (
+          
+                              <a href={"/SelectedCampaign"}>
+                                <span className="ml-3 text-sm text-gray-400">
+                                  {value.category.title}
+                                  </span>
+                              </a>
+                            );
+                          })}
+                        </div>
+              )}
+            </div> 
 
             {tokens &&
               <>
-                {/* <button id="dropdownAvatarNameButton" data-dropdown-toggle="dropdownAvatarName" class="flex items-center text-sm font-medium text-gray-900 rounded-full hover:text-blue-600 dark:hover:text-blue-500 md:mr-0 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:text-white" type="button">
-              <span class="sr-only">Open user menu</span>
-              <img class="mr-2 w-8 h-8 rounded-full" src="https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg" alt="user photo" />
-                Bonnie Green
-                <svg class="w-4 h-4 mx-1.5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-            </button>
-
-            /* <!-- Dropdown menu --> 
-            <div id="dropdownAvatarName" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
-              <div class="py-3 px-4 text-sm text-gray-900 dark:text-white">
-                <div class="font-medium ">Pro User</div>
-                <div class="truncate">name@flowbite.com</div>
-              </div>
-              <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownInformdropdownAvatarNameButtonationButton">
-                <li>
-                  <a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-                </li>
-                <li>
-                  <a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-                </li>
-                <li>
-                  <a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-                </li>
-              </ul>
-              <div class="py-1">
-                <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
-              </div>
-            </div> */}
+ 
 
                 <button
                   className='w-16 ml-8 bg-gray-300 rounded hover:bg-gray-200'
@@ -154,29 +136,6 @@ export default function Header(props) {
               </>
             }
 
-
-            <button
-              data-collapse-toggle="navbar-search"
-              type="button"
-              className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="navbar-search"
-              aria-expanded="false"
-            >
-              <span className="sr-only">Open menu</span>
-              <svg
-                className="w-6 h-6"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg>
-            </button>
           </div>
           <div
             className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
--- helping-hands/components/UsersHomepage.js
+++ helping-hands/components/UsersHomepage.js
@@ -2,120 +2,210 @@ import Feed from "./Feed";
 import PersonalInfo from "./PersonalInfo";
 import Campaigns from "./Campaigns";
 import CampaignsAsPosts from "./CampaignsAsPosts";
+import { useAuth } from '../contexts/Auth'
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import {useRouter} from "next/router";
+
+
 export default function MyPosts() {
-  return (
-    <>
-      {/* Right Section */}
-      <div class="flex justify-center h-screen px-4 text-gray-700">
-        <div class="flex w-full max-w-screen-lg">
-          <div class="flex flex-col w-64 py-4 pr-3">
-            <a
-              class="px-3 py-2 mt-2 text-lg font-medium rounded-sm hover:bg-gray-300"
-              href="#"
-            >
-              Home
-            </a>
-            <a
-              class="px-3 py-2 mt-2 text-lg font-medium rounded-sm hover:bg-gray-300"
-              href="#"
-            >
-              Campaigns
-            </a>
-            <a
-              class="px-3 py-2 mt-2 text-lg font-medium rounded-sm hover:bg-gray-300"
-              href="Campaigns"
-            >
-              Profile
-            </a>
-            <a
-              class="flex px-3 py-2 mt-2 mt-auto text-lg rounded-sm font-medium hover:bg-gray-200"
-              href="#"
-            >
-              <span class="flex-shrink-0 w-10 h-10 bg-gray-400 rounded-full"></span>
-              <div class="flex flex-col ml-2">
-                <span class="mt-1 text-sm font-semibold leading-none">
-                  Username
-                </span>
-                <span class="mt-1 text-xs leading-none">@username</span>
-              </div>
-            </a>
-          </div>
 
-          {/*################# Changing component ##################*/}
+  const { userInfo } = useAuth();
+  const userInfoURL = "https://helping-hands-api.herokuapp.com/api/v1/users/"
+ 
+  const connectionInfoURL = "https://helping-hands-api.herokuapp.com/api/v1/connection/member-campaigns/slug?id="
+ 
+  const [savedData, setSavedData] = useState({});
+  const router = useRouter();
+  const filterData= ( id)=> {
+    console.log(3333,id)
+    console.log(11111,createdCampaigns)
+    createdCampaigns.filter(value =>{
+      
+      if (value.id == id){ 
+        console.log("hello")
+        setSavedData(value)
+      }
+      console.log(2222,value.campaign.id)
 
-          {/* if the user clicks the home button on the left we will display the feed component
-            and if the user clicks the campaigns component we will display the campaignsAsPosts component
-          */}
-          <div class="flex flex-col flex-grow border-l border-r border-gray-300">
-            <div class="flex justify-between flex-shrink-0 px-8 py-4 border-b border-gray-300">
-              <h1 class="text-xl font-semibold">Feed Title</h1>
-              <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-                <button class="flex items-center h-8 px-2 text-sm bg-gray-300 rounded-sm hover:bg-gray-400">
-                  <a href="Createcampain">Create Post</a>
-                </button>
-                <button class="flex items-center h-8 px-2 text-sm bg-gray-300 rounded-sm hover:bg-gray-400">
-                  <a href="Createcampain">Create Campaign</a>
-                </button>
-              </div>
-            </div>
-            <div class="flex-grow h-0 overflow-auto">
-              <CampaignsAsPosts />
-              <Feed />
-            </div>
-          </div>
-
-          {/* if the user clicks on the profile button we will display the personal info component */}
-          {/* <PersonalInfo/> */}
-
-          {/*################# Changing component #########################*/}
-
-          {/* Left */}
-          {/* <div class="flex flex-col flex-shrink-0 w-1/4 py-4 pl-4">
-            <div>
-              <h3 class="mt-6 font-semibold">Section</h3>
-              <div class="flex w-full py-4 border-b border-gray-300">
-                <span class="flex-shrink-0 w-10 h-10 bg-gray-400 rounded-full"></span>
-                <div class="flex flex-col flex-grow ml-2">
-                  <div class="flex text-sm">
-                    <span class="font-semibold">Username</span>
-                    <span class="ml-1">@username</span>
-                  </div>
-                  <p class="mt-1 text-sm">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, et
-                    dolore magna aliqua.
-                  </p>
-                </div>
-              </div>
-              <div class="flex w-full py-4 border-b border-gray-300">
-                <span class="flex-shrink-0 w-10 h-10 bg-gray-400 rounded-full"></span>
-                <div class="flex flex-col flex-grow ml-2">
-                  <div class="flex text-sm">
-                    <span class="font-semibold w-full">Username</span>
-                    <span class="ml-1">@username</span>
-                  </div>
-                  <p class="mt-1 text-sm">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, et
-                    dolore magna aliqua.
-                  </p>
-                </div>
-              </div>
-              <div class="flex w-full py-4 border-b border-gray-300">
-                <span class="flex-shrink-0 w-10 h-10 bg-gray-400 rounded-full"></span>
-                <div class="flex flex-col flex-grow ml-2">
-                  <div class="flex text-sm">
-                    <span class="font-semibold">Username</span>
-                    <span class="ml-1">@username</span>
-                  </div>
-                  <p class="mt-1 text-sm">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, et
-                    dolore magna aliqua.
-                  </p>
-                </div>
-              </div> */}
-            {/* </div>
-          </div> */}
-        </div>
-      </div>
-    </>
-  );
+    })
+    router.push({
+      pathname:"/SelectedCampaign",
+      query:{savedData}
+      })
 }
+
+
+      console.log(66,savedData)
+
+        useEffect(() => {
+          localStorage.setItem('items', JSON.stringify(savedData));
+        }, [savedData]);
+        
+        
+  const [userData, setUserData] = useState();
+  const [connections, setConnections] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const userdata = await axios.get(userInfoURL + userInfo);
+      const connectiondata = await axios.get(connectionInfoURL + userInfo);
+
+      setUserData(userdata.data);
+      setConnections(connectiondata.data);
+    })();
+  }, []);
+
+  // console.log(userData);
+
+  let createdCampaigns = connections.filter(element => element.campaign.organizer.id === userInfo)
+  let joinedCampaigns = connections.filter(element => element.campaign.organizer.id != userInfo)
+  // console.log("c", createdCampaigns);
+  // console.log("j", joinedCampaigns);
+
+  if (createdCampaigns.length > 3){
+    createdCampaigns = createdCampaigns.slice(0, 3)
+  }
+
+  if (joinedCampaigns.length > 3){
+    joinedCampaigns = joinedCampaigns.slice(0, 3)
+  }
+  
+    return (
+      <>
+        {/* Left Section */}
+        <div class="flex justify-center h-screen px-4 text-gray-700">
+          <div class="flex w-full max-w-screen-lg">
+  
+            {/*################# Changing component ##################*/}
+  
+            {/* if the user clicks the home button on the left we will display the feed component
+              and if the user clicks the campaigns component we will display the campaignsAsPosts component
+            */}
+  
+            <div class="flex flex-col flex-grow border-l border-r border-gray-300">
+              <div class="flex justify-between flex-shrink-0 px-8 py-4 border-b border-gray-300">
+                <h1 class="text-xl font-semibold">Feed Title</h1>
+                <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+                  <button class="flex items-center h-8 px-2 text-sm bg-gray-300 rounded-sm hover:bg-gray-400">
+                    <a href="Createcampain">Create Post</a>
+                  </button>
+                  {/* <button class="flex items-center h-8 px-2 text-sm bg-gray-300 rounded-sm hover:bg-gray-400">
+                    <a href="Createcampain">Create Campaign</a>
+                  </button> */}
+                </div>
+              </div>
+  
+              <div class="flex-grow h-0 overflow-auto">
+  
+                {userData &&
+                  <div class="flex w-full p-8 border-b border-gray-300 mt-28">
+                    <div class="relative flex w-full">
+                      {/* <!-- Avatar --> */}
+                      <div class="flex flex-col">
+                        <div style={{ marginTop: '-5rem' }}>
+                          <div style={{ height: '9rem', width: '9rem' }} class="md rounded-full relative avatar">
+  
+                            {/* if(userData != ''){
+                              <img style={{ height: '9rem', width: '9rem' }} class="md rounded-full relative border-4 border-gray-900" src={userData.image} alt="" />
+                            }
+                            else{
+                              <img style={{ height: '9rem', width: '9rem' }} class="md rounded-full relative border-4 border-gray-900" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL_JlCFnIGX5omgjEjgV9F3sBRq14eTERK9w&usqp=CAU" alt="" />
+                            } */}
+                            <img style={{ height: '9rem', width: '9rem' }} class="md rounded-full relative border-4 border-gray-900" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL_JlCFnIGX5omgjEjgV9F3sBRq14eTERK9w&usqp=CAU" alt="" />
+  
+                            <div class="absolute"></div>
+                          </div>
+                        </div>
+                      </div>
+  
+                      <div class="flex flex-1">
+                        {/* <!-- Profile info --> */}
+                        <div class="space-y-2 justify-center w-full ml-3">
+                          {/* <!-- User basic--> */}
+                          <div>
+                            {/* <h2 class="text-xl leading-6 font-bold text-white">ℜ??????ℜ??????.dev</h2> */}
+                            <p class="text-sm leading-5 font-medium text-gray-600">@{userData.username}</p>
+                          </div>
+                          {/* <!-- Description and others --> */}
+                          <div class="mt-3">
+                            <p class="text-black leading-tight mb-2">Description</p>
+                            <div class="text-gray-600 flex">
+                              <span class="flex mr-2">{userData.location}</span>
+                              <span class="flex mr-2"><svg viewBox="0 0 24 24" class="h-5 w-5 paint-icon"><g><path d="M19.708 2H4.292C3.028 2 2 3.028 2 4.292v15.416C2 20.972 3.028 22 4.292 22h15.416C20.972 22 22 20.972 22 19.708V4.292C22 3.028 20.972 2 19.708 2zm.792 17.708c0 .437-.355.792-.792.792H4.292c-.437 0-.792-.355-.792-.792V6.418c0-.437.354-.79.79-.792h15.42c.436 0 .79.355.79.79V19.71z"></path><circle cx="7.032" cy="8.75" r="1.285"></circle><circle cx="7.032" cy="13.156" r="1.285"></circle><circle cx="16.968" cy="8.75" r="1.285"></circle><circle cx="16.968" cy="13.156" r="1.285"></circle><circle cx="12" cy="8.75" r="1.285"></circle><circle cx="12" cy="13.156" r="1.285"></circle><circle cx="7.032" cy="17.486" r="1.285"></circle><circle cx="12" cy="17.486" r="1.285"></circle></g></svg> <span class="leading-5 ml-1">Joined December, 2019</span></span>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+  
+                    </div>
+                  </div>}
+  
+                <CampaignsAsPosts />
+                {/* <Feed /> */}
+              </div>
+            </div>
+  
+            {/* if the user clicks on the profile button we will display the personal info component */}
+            {/* <PersonalInfo/> */}
+  
+            {/*################# Changing component #########################*/}
+  
+            {/* Right */}
+            <div class="flex flex-col flex-shrink-0 w-1/4 py-4 pl-4">
+              <div>
+                <h3 class="mt-6 font-bold">My Campaigns <span><a href="Createcampain" className="text-sm text-blue-400 hover:text-blue-600">Create +</a></span></h3>
+  
+                <h4 class="mt-6 font-semibold">Created <span><a href="/" className="text-blue-400 hover:text-blue-600">see more</a></span> </h4>
+  
+                {
+                 createdCampaigns.map(item => {
+                    return (
+                      <div class="flex w-full py-4 border-b border-gray-300">
+                        <span class="flex-shrink-0 w-10 h-10 bg-gray-400 rounded-full">
+                          <img src="/image/hand-13.jpg"></img>
+                        </span>
+                        <div class="flex flex-col flex-grow ml-2">
+                          <div class="flex text-sm">
+                        <a onClick={() => filterData(item.id)}>    <span class="font-semibold">{item.campaign.title}</span></a>
+                            <span class="ml-1 opacity-50">{item.campaign.date}</span>
+                          </div>
+                          <p class="mt-1 text-sm">
+                            {item.campaign.description.length > 55 ? item.campaign.description.substr(0, 55) + "...." : item.campaign.description}
+                          </p>
+                        </div>
+                      </div>
+                    )
+                  })
+                }
+  
+                <h4 class="mt-6 font-semibold">Joined <span><a href="/" className="text-blue-400 hover:text-blue-600">see more</a></span> </h4>
+  
+                {
+                  joinedCampaigns.map(item => {
+                    return (
+                      <div class="flex w-full py-4 border-b border-gray-300">
+                        <span class="flex-shrink-0 w-10 h-10 bg-gray-400 rounded-full">
+                          <img src="/image/hand-13.jpg"></img>
+                        </span>
+                        <div class="flex flex-col flex-grow ml-2">
+                          <div class="flex text-sm">
+                            <span class="font-semibold">{item.campaign.title}</span>
+                            <span class="ml-1 opacity-50">{item.campaign.date}</span>
+                          </div>
+                          <p class="mt-1 text-sm">
+                            {item.campaign.description.length > 55 ? item.campaign.description.substr(0, 55) + "...." : item.campaign.description}
+                          </p>
+                        </div>
+                      </div>
+                    )
+                  })
+                }
+  
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
